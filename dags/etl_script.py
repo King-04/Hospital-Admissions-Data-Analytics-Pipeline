b58d488059ee37bb1e2a1aa9ea9238d5bbd50042
King-04 <kboateng396@gmail.com>
2024-06-20 13:32:02 +0000

    _

--- dags/etl_script.py
+++ dags/etl_script.py
@@ -8,117 +8,117 @@ import logging
 import cred
 
 
-def extract_transform():
+# def extract_transform():
         
-    # Read the CSV 
-    adm_df = pd.read_csv('source_data/Admission Data.csv')
-    mort_df = pd.read_csv('source_data/Mortality Data.csv')
-    poll_df = pd.read_csv('source_data/Pollution Data.csv')
+#     # Read the CSV 
+#     adm_df = pd.read_csv('source_data/Admission Data.csv')
+#     mort_df = pd.read_csv('source_data/Mortality Data.csv')
+#     poll_df = pd.read_csv('source_data/Pollution Data.csv')
 
-    ## Print the resulting DataFrame
-    # print(adm_df.head(50))
-    # print(mort_df.head(50))
-    # print(poll_df.head(50))
+#     ## Print the resulting DataFrame
+#     # print(adm_df.head(50))
+#     # print(mort_df.head(50))
+#     # print(poll_df.head(50))
         
-    ## Check for missing values and drop them
+#     ## Check for missing values and drop them
 
-    # adm_df.isnull().sum()
+#     # adm_df.isnull().sum()
 
-    # mort_df.isnull().sum()
+#     # mort_df.isnull().sum()
 
-    # poll_df.isnull().sum()
-
-
-    adm_df.fillna(0, inplace=True)
+#     # poll_df.isnull().sum()
 
 
-    poll_df = poll_df.dropna()
+#     adm_df.fillna(0, inplace=True)
 
 
-
-    # # Identify duplicate rows
-    # duplicateRows = adm_df[adm_df.duplicated()]
-    # print(duplicateRows)
-    # duplicateRows = mort_df[adm_df.duplicated()]
-    # print(duplicateRows)
-    # duplicateRows = adm_df[adm_df.duplicated()]
-    # print(duplicateRows)
+#     poll_df = poll_df.dropna()
 
 
 
-    # Changing column to date format
+#     # # Identify duplicate rows
+#     # duplicateRows = adm_df[adm_df.duplicated()]
+#     # print(duplicateRows)
+#     # duplicateRows = mort_df[adm_df.duplicated()]
+#     # print(duplicateRows)
+#     # duplicateRows = adm_df[adm_df.duplicated()]
+#     # print(duplicateRows)
 
 
-    def time_format(df: pd.DataFrame, col: str) -> pd.DataFrame:
-        def parse_with_dayfirst(date_str):
-            if pd.isna(date_str):
-                return pd.NaT
-            try:
-                # Try parsing with month first (default behavior)
-                return parse(date_str)
-            except ValueError:
-                try:
-                    # If month first fails, try with day first
-                    return parse(date_str, dayfirst=True)
-                except ValueError:
-                    # If both fail, return NaT
-                    return pd.NaT
 
-        # Apply the parsing function and ensure the result is datetime
-        df[col] = pd.to_datetime(df[col].apply(parse_with_dayfirst), errors='coerce')
+#     # Changing column to date format
 
-        # Identify rows with invalid dates
-        invalid_dates = df[df[col].isna()]
-        if not invalid_dates.empty:
-            print(f"Warning: {len(invalid_dates)} rows have invalid dates in column {col}.")
-            # print(invalid_dates)
 
-        # Check if all values are NaT
-        if df[col].isna().all():
-            # print(f"Error: All values in column {col} are invalid dates.")
+#     def time_format(df: pd.DataFrame, col: str) -> pd.DataFrame:
+#         def parse_with_dayfirst(date_str):
+#             if pd.isna(date_str):
+#                 return pd.NaT
+#             try:
+#                 # Try parsing with month first (default behavior)
+#                 return parse(date_str)
+#             except ValueError:
+#                 try:
+#                     # If month first fails, try with day first
+#                     return parse(date_str, dayfirst=True)
+#                 except ValueError:
+#                     # If both fail, return NaT
+#                     return pd.NaT
+
+#         # Apply the parsing function and ensure the result is datetime
+#         df[col] = pd.to_datetime(df[col].apply(parse_with_dayfirst), errors='coerce')
+
+#         # Identify rows with invalid dates
+#         invalid_dates = df[df[col].isna()]
+#         if not invalid_dates.empty:
+#             print(f"Warning: {len(invalid_dates)} rows have invalid dates in column {col}.")
+#             # print(invalid_dates)
+
+#         # Check if all values are NaT
+#         if df[col].isna().all():
+#             # print(f"Error: All values in column {col} are invalid dates.")
             
-            # 1. Fill with a default date
-            df[col] = pd.to_datetime('2000-01-01')
-            # 2. Fill with the current date
-            # df[col] = pd.to_datetime(datetime.now().date())
-            # 3. Or just return the DataFrame with NaT values
-            return df
+#             # 1. Fill with a default date
+#             df[col] = pd.to_datetime('2000-01-01')
+#             # 2. Fill with the current date
+#             # df[col] = pd.to_datetime(datetime.now().date())
+#             # 3. Or just return the DataFrame with NaT values
+#             return df
 
-        # Convert datetime values to strings in the desired format
-        df[col] = df[col].dt.strftime('%Y-%m-%d')
+#         # Convert datetime values to strings in the desired format
+#         df[col] = df[col].dt.strftime('%Y-%m-%d')
 
-        return df
-
-
+#         return df
 
 
-    adm_df = time_format(adm_df, 'D.O.A')
-    adm_df = time_format(adm_df, 'D.O.D')
-    adm_df = adm_df.dropna()
 
-    adm_values_to_drop = [3253, 4053, 4357, 4572, 4622, 10165]
-    adm_df = adm_df.drop(adm_df[adm_df['SNO'].isin(adm_values_to_drop)].index)
 
-    # # print(adm_df.head(50))
+#     adm_df = time_format(adm_df, 'D.O.A')
+#     adm_df = time_format(adm_df, 'D.O.D')
+#     adm_df = adm_df.dropna()
 
-    mort_df = time_format(mort_df, 'DATE OF BROUGHT DEAD')
+#     adm_values_to_drop = [3253, 4053, 4357, 4572, 4622, 10165]
+#     adm_df = adm_df.drop(adm_df[adm_df['SNO'].isin(adm_values_to_drop)].index)
 
-    # # print(mort_df.head(50))
+#     # # print(adm_df.head(50))
 
-    poll_df = time_format(poll_df, 'DATE')
-    poll_values_to_drop = ['2018-11-16', '2019-01-23']
-    poll_df = poll_df.drop(poll_df[poll_df['DATE'].isin(poll_values_to_drop)].index)
-    # # print(poll_df.head(50))
+#     mort_df = time_format(mort_df, 'DATE OF BROUGHT DEAD')
 
-    adm_df.isnull().sum()
+#     # # print(mort_df.head(50))
 
-    # mort_df.isnull().sum()
+#     poll_df = time_format(poll_df, 'DATE')
+#     poll_values_to_drop = ['2018-11-16', '2019-01-23']
+#     poll_df = poll_df.drop(poll_df[poll_df['DATE'].isin(poll_values_to_drop)].index)
+#     # # print(poll_df.head(50))
 
-    # poll_df.isnull().sum()
+#     adm_df.isnull().sum()
 
-    # Preprocessed file
+#     # mort_df.isnull().sum()
 
-    return adm_df.to_csv('clean/admission_data.csv', index=False, header=True, sep=','), mort_df.to_csv('clean/mortality_data.csv', index=False, header=True, sep=','), poll_df.to_csv('clean/pollution_data.csv', index=False, header=True, sep=',')
+#     # poll_df.isnull().sum()
+
+#     # Preprocessed file
+
+#     return adm_df.to_csv('clean/admission_data.csv', index=False, header=True, sep=','), mort_df.to_csv('clean/mortality_data.csv', index=False, header=True, sep=','), poll_df.to_csv('clean/pollution_data.csv', index=False, header=True, sep=',')
 
 def load_s3():
         
@@ -130,6 +130,8 @@ def load_s3():
     for local_file in file_name:
         s3.upload_file(local_file, bucket_name, local_file)
         print(f"Uploaded {local_file} to {bucket_name}")
+    
+    return file_name
 
 # # Getting column names to build table
 
@@ -192,3 +194,4 @@ def load_redshift():
     load_data_to_redshift(schema_name, 'admission_data', cred.s3_admis)
     load_data_to_redshift(schema_name, 'mortality_data', cred.s3_mort)
     load_data_to_redshift(schema_name, 'pollution_data', cred.s3_poll)
+
